34da81622f52289a7c22a61819d8e42d7dc7db8f
yrk721 <yoorae0721@naver.com>
2024-08-06 16:32:50 +0900

    feat: 랜딩페이지 스크린 제작 (#2)

--- src/pages/landing/LandingPage.styled.ts
+++ src/pages/landing/LandingPage.styled.ts
@@ -7,5 +7,33 @@ export const LandingContainer = styled.div`
   justify-content: center;
   text-align: center;
   width: 100%;
-  height: 90vh;
+  height: 70vh;
+`;
+
+export const Header = styled.header`
+  display: flex;
+  justify-content: flex-start;
+  width: 100%;
+  padding: 20px 0px 0px 40px;
+`;
+
+export const Logo = styled.img`
+  width: 170px;
+  height: auto;
+`;
+
+export const Title = styled.div`
+  font-size: 60px;
+  font-weight: 700;
+  line-height: 64px;
+`;
+
+export const SubTitle = styled.div`
+  font-size: 20px;
+  font-weight: 400;
+  margin: 32px;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
 `;
--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -1,9 +1,28 @@
+import React from 'react';
+import CustomButton from 'components/common/button/CustomButton';
 import * as S from './LandingPage.styled';
 
 export default function LandingPage() {
   return (
-    <S.LandingContainer>
-      <p>랜딩페이지</p>
-    </S.LandingContainer>
+    <>
+      <S.Header>
+        <S.Logo src="/logo.png" alt="Logo" />
+      </S.Header>
+      <S.LandingContainer>
+        <S.Title>
+          Secure Document <br />
+          Management System
+        </S.Title>
+        <S.SubTitle>Upload, manage, and share your documents securely</S.SubTitle>
+        <S.ButtonContainer>
+          <CustomButton variant="outline" width="320px" height="64px" textColor="#000">
+            Sign Up
+          </CustomButton>
+          <CustomButton variant="default" width="320px" height="64px" textColor="#fff">
+            Login
+          </CustomButton>
+        </S.ButtonContainer>
+      </S.LandingContainer>
+    </>
   );
 }
