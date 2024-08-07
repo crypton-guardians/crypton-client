2e233bc278a1bfad2c6be94130388809394de386
kimDoyun <klll120@naver.com>
2024-08-08 00:03:22 +0900

    * feat: 파일 업로드 및 다운로드 api 틀 추가

--- src/services/apiClient.tsx
+++ src/services/apiClient.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 //로그인 및 회원가입 관련 api 연동 컴포넌트
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost',
+    baseURL: 'http://localhost:8080/api',
     headers: {
         'Content-Type': 'application/json',
     },
