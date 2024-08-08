f2f40603de9657490e82efa2dd21d1645399a254
yrk721 <yoorae0721@naver.com>
2024-08-09 04:48:18 +0900

    feat: font 설정 (#11)

---
+++ src/styles/typography.ts
@@ -0,0 +1,30 @@
+import { css } from '@emotion/react';
+
+const typography = {
+  loginInput: css`
+    font-family: 'Pretendard', sans-serif;
+    font-weight: 600;
+    font-size: 16px;
+    letter-spacing: -0.03em;
+  `,
+  bodyM: css`
+    font-family: 'Pretendard', sans-serif;
+    font-weight: 500;
+    font-size: 18px;
+    letter-spacing: -0.03em;
+  `,
+  bodyS: css`
+    font-family: 'Pretendard', sans-serif;
+    font-weight: 600;
+    font-size: 18px;
+    letter-spacing: -0.03em;
+  `,
+  bodyB: css`
+    font-family: 'Pretendard', sans-serif;
+    font-weight: 700;
+    font-size: 18px;
+    letter-spacing: -0.03em;
+  `,
+};
+
+export default typography;
