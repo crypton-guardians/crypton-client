a45c7275aa86ecfb7bb2286877534262d985d0b8
kai <m01056994380@gmail.com>
2024-08-12 17:16:21 +0900

    chore: 중간 커밋

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -8,7 +8,7 @@ export default function LoginPage() {
     <S.PageContainer>
       <S.ContentsContainer>
         <S.Logo src="/logo-custom.png" alt="Logo" />
-        <S.TextBox>로그인하고 보안 걱정 없는 PDF 열람 서비스를 이용해 보세요</S.TextBox>
+        <S.TextBox>로그인하고 보안 걱정 없는 PDF 열람 서비스를 이용해 보세요</S.TextBox>        
         <S.CustomInput placeholder="이메일 또는 아이디를 입력해 주세요." />
         <S.CustomInput placeholder="비밀번호를 입력해 주세요." />
         <S.AuthButton>로그인</S.AuthButton>
--- src/services/apiClient.tsx
+++ src/services/apiClient.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 //로그인 및 회원가입 관련 api 연동 컴포넌트
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'http://35.237.68.73:8080/api',
     headers: {
         'Content-Type': 'application/json',
     },
