1875657aa9f490d46ef68440073bcd0049c6987a
yrk721 <yoorae0721@naver.com>
2024-08-13 03:43:41 +0900

    feat: 404 페이지 스크린 작업 (#7)

--- src/pages/404/NotFoundPage.styled.ts
+++ src/pages/404/NotFoundPage.styled.ts
@@ -1,9 +1,50 @@
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
 
 export const NotFoundContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: auto;
+  ${({ theme }) => css`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100vh;
+    background-color: ${theme.colors.black[900]};
+  `}
+`;
+
+export const Title = styled.div`
+  padding: 0px 10px 10px 10px;
+  font-size: 72px;
+  font-weight: 700;
+  line-height: 87px;
+  color: white;
+`;
+
+export const SubTitle = styled.div`
+  margin: 20px 0px 50px 0px;
+  font-size: 24px;
+  font-weight: 500;
+  color: white;
+`;
+
+export const HomeButton = styled.button`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 160px;
+    height: 50px;
+    border: none;
+    border-radius: 60px;
+    color: white;
+    ${theme.typography.body1M}
+    transition: background-color 0.5s ease;
+    background: ${theme.colors.gradient.keyGradient};
+    cursor: pointer;
+
+    &:hover {
+      background: ${theme.colors.gradient.keyGradientHover};
+    }
+  `}
 `;
