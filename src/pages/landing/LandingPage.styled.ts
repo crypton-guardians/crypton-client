0670a105a9166b3ce7e63070bcc2e1159e0347dc
yoorae <114637091+yrk721@users.noreply.github.com>
2024-08-13 00:41:10 +0900

    Style/#26 랜딩페이지 디자인 수정 (#28)
    * style: 랜딩페이지 디자인 변경 (#26)
    
    * feat: baseURL 환경변수로 변경 (#26)

--- src/pages/landing/LandingPage.styled.ts
+++ src/pages/landing/LandingPage.styled.ts
@@ -1,4 +1,22 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+
+export const PageContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-position: center;
+  background-size: cover;
+  background-image: url('./background.webp');
+`;
+
+export const Overlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.4);
+`;
 
 export const LandingContainer = styled.div`
   display: flex;
@@ -7,14 +25,16 @@ export const LandingContainer = styled.div`
   justify-content: center;
   text-align: center;
   width: 100%;
-  height: 70vh;
+  height: 95vh;
 `;
 
 export const Header = styled.header`
+  position: absolute;
+  top: 80px;
+  left: 100px;
   display: flex;
   justify-content: flex-start;
   width: 100%;
-  padding: 20px 0px 0px 40px;
 `;
 
 export const Logo = styled.img`
@@ -23,19 +43,67 @@ export const Logo = styled.img`
 `;
 
 export const Title = styled.div`
+  padding: 20px;
   font-size: 60px;
   font-weight: 700;
   line-height: 64px;
+  color: white;
 `;
 
 export const SubTitle = styled.div`
-  font-size: 20px;
-  font-weight: 400;
-  margin: 32px;
+  ${({ theme }) => css`
+    margin: 30px 0px 42px 0px;
+    ${theme.typography.body1M};
+    color: ${theme.colors.black[100]};
+  `}
 `;
 
 export const ButtonContainer = styled.div`
   display: flex;
+`;
+
+export const SignupButton = styled.div`
+  ${({ theme }) => css`
+    ${theme.typography.body1S};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 320px;
+    height: 64px;
+    margin: 10px;
+    border-radius: 10px;
+    border: 1.5px solid white;
+    background-color: transparent;
+    color: white;
+    cursor: pointer;
+    transition: all 0.3s ease;
+
+    &:hover {
+      background-color: rgba(255, 255, 255, 0.1);
+    }
+  `}
+`;
+
+export const LoginButton = styled.div`
+  ${({ theme }) => css`
+    ${theme.typography.body1S};
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 320px;
+    height: 64px;
+    margin: 10px;
+    border: none;
+    border-radius: 10px;
+    background-color: white;
+    color: black;
+    cursor: pointer;
+    transition: all 0.3s ease;
+
+    &:hover {
+      background-color: ${theme.colors.black[200]};
+    }
+  `}
 `;
 
 export const Footer = styled.div`
@@ -48,5 +116,9 @@ export const Footer = styled.div`
 `;
 
 export const FooterItem = styled.div`
-  padding: 30px;
+  ${({ theme }) => css`
+    padding: 30px;
+    ${theme.typography.body3M};
+    color: ${theme.colors.black[400]};
+  `}
 `;
--- src/pages/landing/LandingPage.tsx
+++ src/pages/landing/LandingPage.tsx
@@ -1,45 +1,34 @@
 import * as S from './LandingPage.styled';
 import { useNavigate } from 'react-router-dom';
-import CustomButton from 'components/common/button/CustomButton';
 
 export default function LandingPage() {
   const navigate = useNavigate();
 
   return (
     <>
-      <S.Header>
-        <S.Logo src="/logo-basic.png" alt="Logo" />
-      </S.Header>
-      <S.LandingContainer>
-        <S.Title>
-          Secure Document <br />
-          Management System
-        </S.Title>
-        <S.SubTitle>Upload, manage, and share your documents securely</S.SubTitle>
-        <S.ButtonContainer>
-          <CustomButton
-            variant="outline"
-            width="320px"
-            height="64px"
-            textColor="#000"
-            onClick={() => navigate('/signup')}>
-            Sign Up
-          </CustomButton>
-          <CustomButton
-            variant="default"
-            width="320px"
-            height="64px"
-            textColor="#fff"
-            onClick={() => navigate('/login')}>
-            Login
-          </CustomButton>
-        </S.ButtonContainer>
-        <S.Footer>
-          <S.FooterItem>©2024. Crypton. All rights reserved.</S.FooterItem>
-          <S.FooterItem>Privacy Policy</S.FooterItem>
-          <S.FooterItem>Terms of Service</S.FooterItem>
-        </S.Footer>
-      </S.LandingContainer>
+      <S.PageContainer>
+        <S.Overlay>
+          <S.Header>
+            <S.Logo src="/logo-custom.png" alt="Logo" />
+          </S.Header>
+          <S.LandingContainer>
+            <S.Title>
+              Secure Document <br />
+              Management System
+            </S.Title>
+            <S.SubTitle>로그인하고 보안 걱정없는 PDF 열람 서비스를 이용해 보세요.</S.SubTitle>
+            <S.ButtonContainer>
+              <S.SignupButton onClick={() => navigate('/signup')}>회원가입</S.SignupButton>
+              <S.LoginButton onClick={() => navigate('/login')}>로그인</S.LoginButton>
+            </S.ButtonContainer>
+            <S.Footer>
+              <S.FooterItem>©2024. Crypton. All rights reserved.</S.FooterItem>
+              <S.FooterItem>Privacy Policy</S.FooterItem>
+              <S.FooterItem>Terms of Service</S.FooterItem>
+            </S.Footer>
+          </S.LandingContainer>
+        </S.Overlay>
+      </S.PageContainer>
     </>
   );
 }
