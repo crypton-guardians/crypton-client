b77fb40e71ac491afec36eb8dd633a8aebb8a24c
Do Yun Kim <57212041+dedel009@users.noreply.github.com>
2024-08-12 23:22:18 +0900

    Feat/#9 회원가입/로그인 api 연동 (#25)
    * feat: 브랜치 생성(#9)
    
    * * feat: 로그인 및 회원가입 api 생성
    
    * * feat: 로그인 및 회원가입 api 생성(#9)
    
    * * feat: 파일 업로드 및 다운로드 api 틀 추가
    
    * * feat: 파일 업로드 및 다운로드 api 틀 추가 (#9)
    
    * chore: 중간 커밋
    
    * feat: 회원가입 및 로그인 api 연동 기능 추가(#9)
    
    ---------
    
    Co-authored-by: kimDoyun <klll120@naver.com>

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,20 +1,61 @@
+import { useState } from 'react';
 import * as S from './Auth.styled';
 import { useNavigate } from 'react-router-dom';
+import { login } from 'services/auth/authApi';
 
 export default function LoginPage() {
   const navigate = useNavigate();
+
+  const [loginParams, setLoginParams] = useState({
+    username:"",
+    password:""
+  })
+
+  //로그인 핸들러
+  const handleLogin = async () => {      
+      try{
+        const response = await login(loginParams);
+        console.log("res :::", response)        
+        if(response.status === 200){
+          console.log("로그인 성공")
+          navigate('/dashboard')
+        }
+      }catch(error){
+        console.log("error :::", error)
+        if(error){
+          setLoginParams({username:"", password:""})
+          alert("아이디 및 패스워드가 틀렸습니다. 다시 로그인 해주세요.")          
+        }
+      }      
+  }
 
   return (
     <S.PageContainer>
       <S.ContentsContainer>
         <S.Logo src="/logo-custom.png" alt="Logo" />
-        <S.TextBox>로그인하고 보안 걱정 없는 PDF 열람 서비스를 이용해 보세요</S.TextBox>
-        <S.CustomInput placeholder="이메일 또는 아이디를 입력해 주세요." />
-        <S.CustomInput placeholder="비밀번호를 입력해 주세요." />
-        {/* <S.AuthButton>로그인</S.AuthButton> */}
-
-        {/* NOTE: 로그인 API 연동 전 임시 코드 */}
-        <S.AuthButton onClick={() => navigate('/dashboard')}>로그인</S.AuthButton>
+        <S.TextBox>로그인하고 보안 걱정 없는 PDF 열람 서비스를 이용해 보세요</S.TextBox>        
+        <S.CustomInput 
+        placeholder="이메일 또는 아이디를 입력해 주세요."
+        onChange={(e)=>{
+          setLoginParams({
+            ...loginParams,
+            "username":e.target.value,
+          })
+        }}
+        value={loginParams && loginParams.username}
+        />
+        <S.CustomInput 
+        placeholder="비밀번호를 입력해 주세요."
+        type='password'
+        onChange={(e)=>{
+          setLoginParams({
+            ...loginParams,
+            "password":e.target.value,
+          })
+        }}
+        value={loginParams && loginParams.password}
+        />
+        <S.AuthButton onClick={handleLogin}>로그인</S.AuthButton>
         <S.FooterBox>
           <S.TextBox>아직 크립톤 회원이 아니신가요?</S.TextBox>
           <S.LinkTextBox onClick={() => navigate('/signup')}>회원가입하기</S.LinkTextBox>
---
+++ src/services/apiClient.tsx
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+//로그인 및 회원가입 관련 api 연동 컴포넌트
+
+const apiClient = axios.create({
+    baseURL: 'http://www.crython.shop:8080/api',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+})
+
+export default apiClient;
