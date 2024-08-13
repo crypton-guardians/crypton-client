44e4d863524b54daabff4fa0d7036a63adc9144b
yrk721 <yoorae0721@naver.com>
2024-08-14 04:09:27 +0900

    chore: baseUrl 재변경 (#36)

--- src/services/apiClient.tsx
+++ src/services/apiClient.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://www.crython.shop:8080/api',
+  baseURL: 'http://www.crython.shop:8080/api',
   headers: {
     'Content-Type': 'application/json',
   },
