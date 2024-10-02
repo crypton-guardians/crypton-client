fbcd080e2b5647274348bc92399eeae43a91b2ee
yrk721 <yoorae0721@naver.com>
2024-10-02 20:02:46 +0900

    feat: 레이아웃 수정 (#51)

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,21 +1,15 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import DashboardLayout from 'components/dashboard/layout/DashboardLayout';
 import * as P from 'pages';
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Layout이 필요 없는 페이지 */}
+        <Route path="/" element={<P.LandingPage />} />
         <Route path="/login" element={<P.LoginPage />} />
         <Route path="/signup" element={<P.SignupPage />} />
-        <Route path="/" element={<P.LandingPage />} />
+        <Route path="/dashboard" element={<P.DashboardPage />} />
         <Route path="*" element={<P.NotFoundPage />} />
-
-        {/* Layout이 필요한 페이지 */}
-        <Route element={<DashboardLayout />}>
-          <Route path="/dashboard" element={<P.DashboardPage />} />
-        </Route>
       </Routes>
     </BrowserRouter>
   );
